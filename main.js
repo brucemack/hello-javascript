8ebcaaa8cfb44803da184596479405677c34baa2
Bruce MacKinnon <bruce@Bruces-iMac.local>
2022-11-26 08:18:01 -0500

    Working

---
+++ main.js
@@ -0,0 +1,10 @@
+if (typeof Element.prototype.clearChildren === 'undefined') {
+    Object.defineProperty(Element.prototype, 'clearChildren', {
+      configurable: true,
+      enumerable: false,
+      value: function() {
+        while(this.firstChild) this.removeChild(this.lastChild);
+      }
+    });
+}
+
